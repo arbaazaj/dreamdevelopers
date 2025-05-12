bf0be1ff863a7273bb9ee82a408ea3611d57f3bc
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2025-05-12 05:56:57 +0530

    > Reverting changes due to some errors.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
-const {v4: uuidv4} = require('uuid');
+//const mysql = require('mysql');
+//const bcrypt = require('bcrypt');
+//const cookieParser = require('cookie-parser');
+//const {v4: uuidv4} = require('uuid');
 
 const nodemailer = require("nodemailer");
 const app = express();
@@ -13,29 +13,29 @@ const port = process.env.PORT || 3000;
 const sessions = {};
 
 // MySQL connection
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     connectionLimit: 10,
+// });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+//app.use(cookieParser());
 
 // Middleware to check if user is logged in
-const requireLogin = (req, res, next) => {
-    const sessionId = req.cookies && req.cookies.sessionId;
-    if (sessionId || sessions[sessionId]) {
-        req.user = sessions[sessionId].user;
-        next();
-    } else {
-        res.redirect('/login');
-    }
-};
+// const requireLogin = (req, res, next) => {
+//     const sessionId = req.cookies && req.cookies.sessionId;
+//     if (sessionId || sessions[sessionId]) {
+//         req.user = sessions[sessionId].user;
+//         next();
+//     } else {
+//         res.redirect('/login');
+//     }
+// };
 
 // Root endpoint
 app.get('/', (req, res) => {
@@ -73,76 +73,76 @@ app.get('/dashboard', (req, res) => {
 });
 
 // Login post request
-app.post('/login', (req, res) => {
-    const {email, password} = req.body;
-
-    pool.query('SELECT * FROM students WHERE email = ?', [email], async (error, results) => {
-        if (error) {
-            console.error('Error during login:', error);
-            return res.send('Error logging in.');
-        }
-        if (results.length > 0) {
-            const user = results[0];
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (passwordMatch) {
-                const sessionId = uuidv4();
-                sessions[sessionId] = {user: {id: user.id, email: user.email}};
-                res.cookies('sessionId', sessionId, {httpOnly: true});
-                return res.redirect('/dashboard');
-            } else {
-                return res.send('Invalid username or password.');
-            }
-        } else {
-            return res.send('Invalid credentials');
-        }
-    });
-
-    if (email === 'admin@dd.dev' && password === 'password') {
-        res.redirect('/dashboard');
-    } else {
-        res.send('Invalid username or password.');
-    }
-});
-
-// Register post request
-app.post('/register', (req, res) => {
-    const {name, email, password, confirm_password} = req.body;
-    const studentId = uuidv4();
-
-    if (password !== confirm_password) {
-        return res.status(400).send('Passwords do not match.');
-    }
-
-    try {
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        pool.query('INSERT INTO students (id, name, email, password) VALUES (?, ?, ?, ?)', [studentId, name, email, hashedPassword], (error) => {
-            if (error) {
-                console.error('Error during registration:', error);
-                if (error.code === 'ER_DUP_ENTRY') {
-                    return res.status(400).send('Email already exists.');
-                }
-                return res.status(500).send('Error registering user.');
-            }
-            res.redirect('/login');
-        });
-    } catch (error) {
-        console.error('Error hashing password:', error);
-        res.send('Error registering user.');
-    }
-
-    res.send('Registration successful!');
-    res.redirect('/dashboard');
-});
+// app.post('/login', (req, res) => {
+//     const {email, password} = req.body;
+//
+//     pool.query('SELECT * FROM students WHERE email = ?', [email], async (error, results) => {
+//         if (error) {
+//             console.error('Error during login:', error);
+//             return res.send('Error logging in.');
+//         }
+//         if (results.length > 0) {
+//             const user = results[0];
+//             const passwordMatch = await bcrypt.compare(password, user.password);
+//             if (passwordMatch) {
+//                 const sessionId = uuidv4();
+//                 sessions[sessionId] = {user: {id: user.id, email: user.email}};
+//                 res.cookies('sessionId', sessionId, {httpOnly: true});
+//                 return res.redirect('/dashboard');
+//             } else {
+//                 return res.send('Invalid username or password.');
+//             }
+//         } else {
+//             return res.send('Invalid credentials');
+//         }
+//     });
+//
+//     if (email === 'admin@dd.dev' && password === 'password') {
+//         res.redirect('/dashboard');
+//     } else {
+//         res.send('Invalid username or password.');
+//     }
+// });
+//
+// // Register post request
+// app.post('/register', (req, res) => {
+//     const {name, email, password, confirm_password} = req.body;
+//     const studentId = uuidv4();
+//
+//     if (password !== confirm_password) {
+//         return res.status(400).send('Passwords do not match.');
+//     }
+//
+//     try {
+//         const hashedPassword = bcrypt.hashSync(password, 10);
+//         pool.query('INSERT INTO students (id, name, email, password) VALUES (?, ?, ?, ?)', [studentId, name, email, hashedPassword], (error) => {
+//             if (error) {
+//                 console.error('Error during registration:', error);
+//                 if (error.code === 'ER_DUP_ENTRY') {
+//                     return res.status(400).send('Email already exists.');
+//                 }
+//                 return res.status(500).send('Error registering user.');
+//             }
+//             res.redirect('/login');
+//         });
+//     } catch (error) {
+//         console.error('Error hashing password:', error);
+//         res.send('Error registering user.');
+//     }
+//
+//     res.send('Registration successful!');
+//     res.redirect('/dashboard');
+// });
 
 // Logout route
-app.get('/logout', (req, res) => {
-    const sessionId = req.cookies && req.cookies.sessionId;
-    if (sessionId && sessions[sessionId]) {
-        delete sessions[sessionId];
-        res.clearCookie('sessionId');
-    }
-    res.redirect('/');
-});
+// app.get('/logout', (req, res) => {
+//     const sessionId = req.cookies && req.cookies.sessionId;
+//     if (sessionId && sessions[sessionId]) {
+//         delete sessions[sessionId];
+//         res.clearCookie('sessionId');
+//     }
+//     res.redirect('/');
+// });
 
 // Send-email route for contact form
 app.post('/send-email', express.urlencoded({extended: false}), (req, res) => {
